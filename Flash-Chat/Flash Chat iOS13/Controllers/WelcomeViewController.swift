01b05908ef99b7b0f508250fdb908806a4e21a67
Pieprz <Pieprz@users.noreply.github.com>
2021-03-21 01:03:00 +0300

    Remove CLTypingLabel, Add Firebase

--- Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -17,14 +17,12 @@ class WelcomeViewController: UIViewController {
 		
 		titleLabel.text = ""
 		var charIndex = 0.0
-		let titleText = "⚡️FlashChat"
-		for letter in titleText {
+		let textTitle = "⚡️FlashChat"
+		for letter in textTitle {
 			Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
 				self.titleLabel.text?.append(letter)
 			}
 			charIndex += 1
 		}
-    }
-    
-
+	}
 }
